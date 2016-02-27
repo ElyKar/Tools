1f97df8ec370d48d426df75d240912e3f3c6d443
ElyKar <tristan.claverie@laposte.net>
2016-02-27 12:34:50 +0100

    Add an bag made of an array

---
+++ Ruby/array_bag.rb
@@ -0,0 +1,32 @@
+# Implementation of a bag using an array
+# It supports the usual add and each operation
+#
+# Author:: Tristan Claverie
+# License:: MIT
+class ArrayBag
+
+    # Initialize an empty bag
+    def initialize
+        @elements = []
+    end
+
+    # Size of the bag
+    def size?
+        @elements.size
+    end
+
+    # Is the bag empty ?
+    def empty?
+        @elements.empty?
+    end
+
+    # Add an element
+    def add(element)
+        @elements << element
+    end
+
+    # Iterate through the elements
+    def each(&block)
+        @elements.each(&block)
+    end
+end
