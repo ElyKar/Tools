68a56326affc6ed41b6e66c20933d8195a792521
ElyKar <tristan.claverie@laposte.net>
2016-03-12 18:25:04 +0100

    Add InsertionHalf and InsertionBinary

--- Ruby/sorting/insertion.rb
+++ Ruby/sorting/insertion.rb
@@ -11,10 +11,9 @@ class Insertion
     # insertion sort
     def self.sort!(a)
         (1..a.size-1).each do |i|
-            j = i-1
-            while j >= 0 and (a[j] <=> a[i]) > 0
-                a[j], a[i] = a[i], a[j]
-                i, j = j, j-1
+            while i-1 >= 0 and (a[i-1] <=> a[i]) > 0
+                a[i-1], a[i] = a[i], a[i-1]
+                i -= 1
             end
         end
     end
@@ -22,7 +21,84 @@ class Insertion
     # Returns a sorted copy of the array in parameter
     # using insertion sort
     def self.sort(a)
-        sorted = Array.new(a)
+        sorted = a.dup
+        self.sort!(sorted)
+        return sorted
+    end
+
+end
+
+# InsertionHalf handles insertion sort in ascendant order using half exchanges.
+# It allows to reduce the number of exchanges and represent a good optimization.
+# It provides two methods, one for sorting the array in parameter and
+# the other to return a sorted copy of the array
+# The items of the array must support the <=> operators
+#
+# Author:: Tristan Claverie
+# License:: MIT
+class InsertionHalf
+
+    # Sort the given array in parameter using
+    # insertion sort
+    def self.sort!(a)
+        (1..a.size-1).each do |i|
+            current = a[i]
+            while i-1 >= 0 and (a[i-1] <=> current) > 0
+                a[i] = a[i-1]
+                i -= 1
+            end
+            a[i] = current
+        end
+    end
+
+    # Returns a sorted copy of the array in parameter
+    # using insertion sort
+    def self.sort(a)
+        sorted = a.dup
+        self.sort!(sorted)
+        return sorted
+    end
+
+end
+
+# InsertionBinary handles insertion sort in ascendant order using binary search
+# to find the right position and half exchanges.
+# It allows to reduce the number of exchanges and comparisons and represent a good optimization.
+# It provides two methods, one for sorting the array in parameter and
+# the other to return a sorted copy of the array
+# The items of the array must support the <=> operators
+#
+# Author:: Tristan Claverie
+# License:: MIT
+class InsertionBinary
+
+    # Sort the given array in parameter using
+    # insertion sort
+    def self.sort!(a)
+        (1..a.size-1).each do |i|
+            current = a[i]
+            lo, hi = 0, i
+            while lo < hi
+                mid = lo + (hi - lo) / 2
+                if (current <=> a[mid]) < 0
+                    hi = mid
+                else
+                    lo = mid + 1
+                end
+            end
+
+            while i > lo
+                a[i] = a[i-1]
+                i -= 1
+            end
+            a[i] = current
+        end
+    end
+
+    # Returns a sorted copy of the array in parameter
+    # using insertion sort
+    def self.sort(a)
+        sorted = a.dup
         self.sort!(sorted)
         return sorted
     end
