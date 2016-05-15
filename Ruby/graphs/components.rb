7bed8f0d7a9cf0f169eb377d65893a973f2cc6a8
ElyKar <tristan.claverie@laposte.net>
2016-05-15 09:29:07 +0200

    Add kosaraju sharir algorithm

--- Ruby/graphs/components.rb
+++ Ruby/graphs/components.rb
@@ -32,8 +32,7 @@ class Component
 
     # Get all the components
     def components
-        arr = Arrays.new(@nb)
-        (0...@nb).each { |i| arr[i] = [] }
+        arr = Arrays.new(@nb) { [] }
         (0...@component.size).each { |v| arr[@component[v]] << v }
     end
 
@@ -55,4 +54,84 @@ class Component
         end
     end
 
+end
+
+# Finds the strong components in a graph using kosaraju sharir algorithm.
+# It relies on the fact that strong components are the same between a digraph and its
+# reversed digraph (invert all edges)
+# Principle is as follow:
+# - Do a first dfs storing traversed the nodes in a stack by order of visit on the reversed digraph
+# - Iterate through the stack in reverse order (first inserted is first retrieved)
+#   and perform a DFS on the graph to find the component
+# - All done !
+# Some functions are provided to query about the structure of the digraph
+#
+# Author:: Tristan Claverie
+# License:: MIT
+class KosarajuSharirSC
+
+    # Computes the strong components of the given graph
+    def initialize(graph)
+        @nb = 0
+        @component = Array.new(graph.V)
+        @visited = Array.new(graph.V, false)
+        @stack = []
+        reverse = graph.reverse
+        (0...graph.V).each { |v| dfs_stack(reverse, v) if not @visited[v] }
+        @visited.fill(false) # Reset the array
+        (0...graph.V).each do |v|
+            if not @visited[v]
+                dfs(graph, v)
+                @nb += 1
+            end
+        end
+    end
+
+    # Number of connected components
+    def C
+        @nb
+    end
+
+    # Get the component of v
+    def component_of(v)
+        @component[v]
+    end
+
+    # Get all the components
+    def components
+        arr = Arrays.new(@nb) { [] }
+        (0...@component.size).each { |v| arr[@component[v]] << v }
+    end
+
+    private
+
+    # First dfs, stores the nodes traversed in a stack
+    def dfs_stack(graph, v)
+        @visited[v] = true
+        w = nil
+        @stack.push(v)
+        graph.adj(v).each do |e|
+            w = e.to
+            dfs_stack(graph, w) if not @visited[w]
+        end
+    end
+
+    # Fill the components array
+    def dfs(graph, v)
+        @visited[v] = true
+        w = nil
+        @component[v] = @nb
+        graph.adj(v).each do |e|
+            w = e.to
+            dfs(graph, w) if not @visited[w]
+        end
+    end
+
+end
+
+class TarjanSC
+
+    def initialize(argument)
+        @argument = argument
+    end
 end
